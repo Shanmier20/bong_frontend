9ec24810259d71918bfa93732df1ad5e886ff904
Shanmier20 <santosmier16@gmail.com>
2025-10-31 19:18:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Set up the proxy to forward /api requests to the Express backend
+   allowedHosts: [
+      "bong-frontend.vercel.app", // ✅ your live Vercel domain
+    ],
     proxy: {
       "/api": {
         target: "http://localhost:3000", // Your Express server's address
